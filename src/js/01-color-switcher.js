2048b3dd42af5104bb46b03a2b8d4d95ca4aa89f
Aratskov <ifromukraine2022@gmail.com>
2023-02-24 11:15:51 +0200

    init

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyRef = document.querySelector('body');
+const startRef = document.querySelector('[data-start]');
+const stopRef = document.querySelector('[data-stop]');
 
+startRef.addEventListener('click', clickStart);
+stopRef.addEventListener('click', clickStop);
+
+let intervalId = null;
+
+function clickStart() {
+  startRef.disabled = true;
+  stopRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function clickStop() {
+  clearInterval(intervalId);
+  startRef.disabled = false;
+  stopRef.disabled = true;
+}

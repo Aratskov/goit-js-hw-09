8354066f3f985d29b5bd47dcfd7cd53fa27f7075
Aratskov <ifromukraine2022@gmail.com>
2023-02-25 11:26:32 +0200

    2

---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let value = 0;
+
+const buttonStart = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+buttonStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+      return;
+    }
+
+    value = selectedDates[0];
+    buttonStart.disabled = false;
+  },
+};
+
+class Timer {
+  constructor() {
+    this.intervalId = null;
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentTime = new Date();
+      const deltaTime = value - currentTime;
+      if (deltaTime < 0) {
+        clearInterval(this.intervalId);
+        buttonStart.disabled = true;
+        return;
+      }
+      const time = convertMs(deltaTime);
+      updateClockface(time);
+    }, 1000);
+  }
+}
+
+const timer = new Timer();
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minutesRef.textContent = `${minutes}`;
+  secondsRef.textContent = `${seconds}`;
+}
+
+flatpickr('input[type="text"]', options);
+
+buttonStart.addEventListener('click', timer.start.bind(timer));
